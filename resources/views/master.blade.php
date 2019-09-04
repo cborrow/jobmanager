b06ae662df72d5828fe3bf6e0e53fd4efb45302d
Cory Borrow <cborrow03@gmail.com>
2019-09-04 17:56:10 -0400

    First commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<title>Job Manager</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}" />
+</head>
+<body>
+	<div class="container">
+
+	</div>
+</body>
+</html>
