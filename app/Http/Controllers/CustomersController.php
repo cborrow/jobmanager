497b972c4b9680dbcf9f035dd23a80c9e2135472
Cory Borrow <cborrow03@gmail.com>
2019-09-07 13:26:20 -0400

    Updated CustomersController
    Changed default customers per page for index.
    Moved code to add and update customer to seperate functions that return json for use with API later.

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class CustomersController extends Controller
 {
     public function index() {
-    	/*$customers = Customer::paginate(50);
+    	/*$customers = Customer::paginate(30);
     	return view('customers.index', compact('customers'));*/
 
         return view('customers.index');
@@ -30,37 +30,12 @@ class CustomersController extends Controller
     }
 
     public function insert(Request $request) {
-    	$customer = new Customer();
-    	$customer->first_name = $request->firstName;
-    	$customer->last_name = $request->lastName;
-    	$customer->company = $request->company;
-    	$customer->phone = $request->phone;
-    	$customer->alt_phone = $request->altPhone;
-    	$customer->email = $request->email;
-    	$customer->address = $request->address;
-    	$customer->city = $request->city;
-    	$customer->state = $request->state;
-    	$customer->zip = $request->zip;
-    	$customer->notes = $request->notes;
-    	$customer->save();
-
+    	addCustomer($request);
     	return redirect('/customers');
     }
 
     public function update(Request $request, Customer $customer) {
-    	$customer->first_name = $request->firstName;
-    	$customer->last_name = $request->lastName;
-    	$customer->company = $request->company;
-    	$customer->phone = $request->phone;
-    	$customer->alt_phone = $request->alt_phone;
-    	$customer->email = $request->email;
-    	$customer->address = $request->address;
-    	$customer->city = $request->city;
-    	$customer->state = $request->state;
-    	$customer->zip = $request->zip;
-    	$customer->notes = $request->notes;
-    	$customer->save();
-
+    	updateCustomer($request, $customer);
     	return redirect('/customers/' . $customer->id);
     }
 
@@ -74,5 +49,42 @@ class CustomersController extends Controller
 
     public function getCustomer(Customer $customer) {
     	return json_encode($customer);
+    }
+
+    public function addCustomer(Request $request) {
+    	$customer = new Customer();
+    	$customer->first_name = $request->firstName;
+    	$customer->last_name = $request->lastName;
+    	$customer->company = $request->company;
+    	$customer->phone = $request->phone;
+    	$customer->alt_phone = $request->altPhone;
+    	$customer->email = $request->email;
+    	$customer->address = $request->address;
+    	$customer->city = $request->city;
+    	$customer->state = $request->state;
+    	$customer->zip = $request->zip;
+    	$customer->note = $request->notes;
+    	if($customer->save()) {
+    		return json_encode(['status' => 'true']);
+    	}
+    	return json_encode(['status' => 'false']);
+    }
+
+    public function updateCustomer(Request $request, Customer $customer) {
+    	$customer->first_name = $request->firstName;
+    	$customer->last_name = $request->lastName;
+    	$customer->company = $request->company;
+    	$customer->phone = $request->phone;
+    	$customer->alt_phone = $request->alt_phone;
+    	$customer->email = $request->email;
+    	$customer->address = $request->address;
+    	$customer->city = $request->city;
+    	$customer->state = $request->state;
+    	$customer->zip = $request->zip;
+    	$customer->note = $request->notes;
+    	if($customer->save()) {
+    		return json_encode(['status' => 'true', 'customer' => $customer->id]);
+    	}
+    	return json_encode(['status' => 'false', 'customer' => $customer->id]);
     }
 }
